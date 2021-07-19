ec508fd95bfea6679b51315ff8dc9c1dea5d8a91
SamuelsEdwin <samuelsedwin.js@gmail.com>
2021-07-19 03:23:24 +0200

    R-T update active and passive fix

--- src/Scrapper.js
+++ src/Scrapper.js
@@ -462,7 +462,7 @@ function upsertChampionDetails(champ ) {
     try{
         let hasStoredResources = fileUtil.fileExists({filename: champ.name, isImage: false, isJson: true}).jsonExists;
 
-        hasStoredResources = !champ.name.trim().startsWith('Q');
+        hasStoredResources = !champ.name.trim().startsWith('T');
 
         if(hasStoredResources === false) {
             console.log(champ.name);
