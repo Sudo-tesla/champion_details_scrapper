26d4cf4f71ca5c2372ab7a4d3935c938bbadeccd
SamuelsEdwin <samuelsedwin.js@gmail.com>
2021-08-06 03:34:03 +0200

    updated T

--- src/Scrapper.js
+++ src/Scrapper.js
@@ -466,7 +466,7 @@ function upsertChampionDetails(champ ) {
     try{
         let hasStoredResources = fileUtil.fileExists({filename: champ.name, isImage: false, isJson: true}).jsonExists;
 
-        hasStoredResources = !champ.name.trim().startsWith('Z');
+        hasStoredResources = !champ.name.trim().startsWith('T');
 
         if(hasStoredResources === false) {
             //console.log(champ.name);
